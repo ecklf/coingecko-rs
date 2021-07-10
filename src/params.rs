a5a2a5b64b719b64ea1dc1c6add0e4822548139d
ecklf <ecklf@icloud.com>
2021-07-10 18:24:51 +0200

    docs: add params doc

--- src/params.rs
+++ src/params.rs
@@ -1,55 +1,97 @@
+/// Market display order for `coins_markets`
 pub enum MarketsOrder {
+    /// Marketcap descending
     MarketCapDesc,
+    /// Marketcap ascending
     MarketCapAsc,
+    /// Coingecko descending
     GeckoDesc,
+    /// Coingecko ascending
     GeckoAsc,
+    /// Volume descending
     VolumeDesc,
+    /// Volume ascending
     VolumeAsc,
+    /// ID descending
     IdDesc,
+    /// ID ascending
     IdAsc,
 }
 
+/// Price change percentage times for `coins_markets`
 pub enum PriceChangePercentage {
+    /// 1h
     OneHour,
+    /// 24h
     TwentyFourHours,
+    /// 7d
     SevenDays,
+    /// 14d
     FourteenDays,
+    /// 30d
     ThirtyDays,
+    /// 200d
     TwoHundredDays,
+    /// 1y
     OneYear,
 }
 
+/// Tickers order for `coin_tickers` and `exchange_tickers`
 pub enum TickersOrder {
+    /// Trust Score ascending
     TrustScoreAsc,
+    /// Trust Score descending
     TrustScoreDesc,
+    /// Volume descending
     VolumeDesc,
 }
 
+/// Ohlc times for `coin_ohlc`
 pub enum OhlcDays {
+    /// 1d
     OneDay,
+    /// 7d
     SevenDays,
+    /// 14d
     FourteenDays,
+    /// 30d
     ThirtyDays,
+    /// 90d
     NinetyDays,
+    /// 180d
     OneHundredEightyDays,
+    /// 365
     ThreeHundredSixtyFiveDays,
 }
 
+/// Tickers to include for `derivatives` and `derivatives_exchange`
 pub enum DerivativesIncludeTickers {
+    /// All tickers
     All,
+    /// Unexpired tickers
     Unexpired,
 }
 
+/// Order of exchanges for `derivative_exchanges`
 pub enum DerivativeExchangeOrder {
+    /// Name ascending
     NameAsc,
+    /// Name descending
     NameDesc,
+    /// Open interest BTC ascending
     OpenInterestBtcAsc,
+    /// Open interest BTC descending
     OpenInterestBtcDesc,
+    /// 24h BTC trade volume ascending
     TradeVolume24hBtcAsc,
+    /// 24h BTC trade volume descending
     TradeVolume24hBtcDesc,
 }
 
+/// IDs for coins held in treasury for `companies`
 pub enum CompaniesCoinId {
+    /// Bitcoin
     Bitcoin,
+    /// Ethereum
     Ethereum,
 }
